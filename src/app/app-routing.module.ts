4f8bff88207bd4ec4ac5309d8019f646482d67f0
Sierra Parker <parkersierra40@gmail.com>
2024-01-23 12:41:16 -0500

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { HomeComponent } from './outline/home/home.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
 import { DessertComponent } from './pages/dessert/dessert.component';
 import { FavsComponent } from './pages/favs/favs.component';
-import { FamilyFavsComponent } from './pages/quick dinner/family-favs.component';
 import { ResturantsComponent } from './pages/resturants/resturants.component';
 import { FooterComponent } from './outline/footer/footer.component';
 import { NavComponent } from './outline/nav/nav.component';
@@ -15,6 +14,7 @@ import { TiktokComponent } from './pages/tiktok/tiktok.component';
 import { AppsComponent } from './pages/apps/apps.component';
 import { VeganComponent } from './pages/vegan/vegan.component';
 import { NolaComponent } from './pages/nola/nola.component';
+import { QuickdinnerComponent } from './pages/quickdinner/quickdinner.component';
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent}, // Display HeaderComponent on the empty path
@@ -22,7 +22,6 @@ const routes: Routes = [
   { path: 'Pop-Up', component: PopUpComponent },
   { path: 'Dessert', component: DessertComponent },
   { path: 'Favs', component: FavsComponent },
-  { path: 'Family', component: FamilyFavsComponent },
   { path: 'Resturants', component: ResturantsComponent },
   { path: 'Footer', component: FooterComponent},
   { path: 'Nav', component: NavComponent},
@@ -31,7 +30,8 @@ const routes: Routes = [
   { path: 'tiktok', component: TiktokComponent},
   { path: 'apps', component: AppsComponent},
   { path: 'vegan', component: VeganComponent},
-  { path: 'nola', component: NolaComponent}
+  { path: 'nola', component: NolaComponent},
+  {path: 'quickdinner', component:QuickdinnerComponent}
 
 ];
 
